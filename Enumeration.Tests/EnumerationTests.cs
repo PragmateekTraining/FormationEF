08ad2640b8c099a17d24ae5ee236c476d4c3d2e1
Pragmateek <postmaster@pragmateek.com>
2014-07-06 01:05:14 +0200

    Add basic enumeration sample

---
+++ Enumeration.Tests/EnumerationTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity;
+using System.Diagnostics;
+
+namespace Enumeration.Tests
+{
+    class Number
+    {
+        public long ID { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    class Context : DbContext
+    {
+        public DbSet<Number> Numbers { get; set; }
+    }
+
+    [TestClass]
+    public class EnumerationTests
+    {
+        [TestMethod]
+        public void CanEnumerateEntities()
+        {
+            using (Context context = new Context())
+            {
+                foreach (string name in new[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Height", "Nine", "Ten" })
+                {
+                    context.Numbers.Add(new Number { Name = name });
+
+                    context.SaveChanges();
+                }
+            }
+
+            using (Context context = new Context())
+            {
+                context.Database.Log += sql => Debug.WriteLine(sql);
+
+                foreach (Number number in context.Numbers)
+                {
+                    Debug.WriteLine(number.Name);
+                }
+            }
+        }
+    }
+}
