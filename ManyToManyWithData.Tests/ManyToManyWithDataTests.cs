a1f3b749be73ace911b8db7c538734cde30634aa
Pragmateek <postmaster@pragmateek.com>
2014-07-06 19:10:14 +0200

    Add many2many relation with data sample (good database first WF sample)

---
+++ ManyToManyWithData.Tests/ManyToManyWithDataTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace ManyToManyWithData.Tests
+{
+    [TestClass]
+    public class ManyToManyWithDataTests
+    {
+        [TestMethod]
+        public void CanRetrieveData()
+        {
+            using (MoviesContext context = new MoviesContext())
+            {
+                foreach (movie movie in context.movies)
+                {
+                    Debug.WriteLine("{0} :", (object)movie.Title);
+
+                    foreach (performance performance in movie.performances)
+                    {
+                        Debug.WriteLine("\t{0}: {1}", performance.actor.Name, performance.fee);
+                    }
+                }
+
+                Debug.WriteLine(new string('=', 20));
+
+                foreach (actor actor in context.actors)
+                {
+                    Debug.WriteLine("{0} :", (object)actor.Name);
+
+                    foreach (performance performance in actor.performances)
+                    {
+                        Debug.WriteLine("\t{0}: {1}", performance.movie.Title, performance.fee);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ManyToManyWithData.Tests/movies.sql
@@ -0,0 +1,35 @@
+﻿-- IF db_id('movies') IS NOT NULL
+
+-- CREATE DATABASE movies
+
+USE movies
+
+CREATE TABLE movies
+(
+	id INT PRIMARY KEY IDENTITY,
+	title TEXT
+)
+
+CREATE TABLE actors
+(
+	id INT PRIMARY KEY IDENTITY,
+	name TEXT
+)
+
+CREATE TABLE performances
+(
+	movie_id INT NOT NULL FOREIGN KEY REFERENCES movies(id),
+	actor_id INT NOT NULL FOREIGN KEY REFERENCES actors(id),
+	fee INT NOT NULL
+)
+
+INSERT INTO movies(title) VALUES ('Mission: Impossible')
+INSERT INTO movies(title) VALUES ('Matrix')
+INSERT INTO movies(title) VALUES ('War of the Worlds')
+
+INSERT INTO actors(name) VALUES ('Tom Cruise')
+INSERT INTO actors(name) VALUES ('Keanu Reeves')
+
+INSERT INTO performances(movie_id, actor_id, fee) VALUES (1, 1, 290000000)
+INSERT INTO performances(movie_id, actor_id, fee) VALUES (2, 2, 262000000)
+INSERT INTO performances(movie_id, actor_id, fee) VALUES (3, 1, 100000000)
