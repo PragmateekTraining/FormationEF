b3f8305cdf8a275633f869e1f8f6d02c7fc68257
Pragmateek <postmaster@pragmateek.com>
2014-07-09 12:45:52 +0200

    Add case-sensitivity and relationships buildup tests + refactor them

--- Queries.Tests/QueriesTests.cs
+++ Queries.Tests/QueriesTests.cs
@@ -36,8 +36,8 @@ namespace Queries.Tests
     [TestClass]
     public class QueriesTests
     {
-        [TestMethod]
-        public void CanQueryWithLINQToEntities()
+        [ClassInitialize]
+        public static void SetUp(TestContext _)
         {
             Department marketing = new Department
             {
@@ -46,8 +46,13 @@ namespace Queries.Tests
                 {
                     new Employee
                     {
-                        Name = "Tracy Boobs",
+                        Name = "Jane Doe",
                         HireDate = new DateTime(2010, 01, 01)
+                    },
+                    new Employee
+                    {
+                        Name = "John Doe",
+                        HireDate = new DateTime(2001, 01, 01)
                     }
                 }
             };
@@ -64,7 +69,51 @@ namespace Queries.Tests
 
                 context.SaveChanges();
             }
+        }
 
+        [TestMethod]
+        public void IsCaseInsensitive()
+        {
+            using (HRContext context = new HRContext())
+            {
+                Department marketing = context.Departments.Single(dpt => dpt.Name == "mArKeTiNg");
+
+                Assert.IsNotNull(marketing);
+
+                Assert.AreEqual("Marketing", marketing.Name);
+
+                Employee John = context.Employees.Single(e => e.Name == "john doe");
+
+                Assert.IsNotNull(John);
+
+                Assert.AreEqual("John Doe", John.Name);
+            }
+        }
+
+        [TestMethod]
+        public void IsImplicitlyBuildingRelationships()
+        {
+            using (HRContext context = new HRContext())
+            {
+                Department mrktng = context.Departments.Single(dpt => dpt.Name == "Marketing");
+
+                Assert.IsNotNull(mrktng);
+
+                Assert.IsNull(mrktng.Employees);
+
+                Employee John = context.Employees.Single(e => e.Name == "John Doe");
+
+                Assert.IsNotNull(mrktng.Employees);
+
+                Assert.AreEqual(1, mrktng.Employees.Count);
+
+                Assert.AreEqual("John Doe", mrktng.Employees[0].Name);
+            }
+        }
+
+        [TestMethod]
+        public void CanQueryWithLINQToEntities()
+        {
             using (HRContext context = new HRContext())
             {
                 context.Database.Log += sql => Debug.WriteLine(sql);
@@ -100,6 +149,21 @@ namespace Queries.Tests
                 Debug.WriteLine("===== Contains =====");
 
                 context.Employees.Where(e => new[] { "Paris", "London", null }.Contains(e.City)).ToList();
+            }
+
+            using (HRContext context = new HRContext())
+            {
+                context.Database.Log += sql => Debug.WriteLine(sql);
+
+                var results = context.Departments.Select(dpt => new { Department = dpt, RecentEmployees = dpt.Employees.Where(e => e.HireDate > new DateTime(2009, 01, 01)) }).ToList();
+
+                Assert.AreEqual(2, results.Count);
+
+                Assert.AreEqual("Marketing", results[0].Department.Name);
+
+                Assert.AreEqual(1, results[0].Department.Employees.Count);
+
+                Assert.AreEqual("Jane Doe", results[0].Department.Employees[0].Name);
             }
         }
     }
