843f873a9f6f0cc46dfbb99dda5ddadc4e6a93c9
Pragmateek <postmaster@pragmateek.com>
2014-07-09 20:10:34 +0200

    Ensure SQLite DB is created

--- RepositoryPatternSample/EFSQLiteQuestionBasesRepository.cs
+++ RepositoryPatternSample/EFSQLiteQuestionBasesRepository.cs
@@ -3,6 +3,8 @@ using System.Data.Entity;
 using System.Linq;
 using Model;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data;
+using System.Data.SQLite;
 
 namespace Repositories
 {
@@ -27,6 +29,29 @@ namespace Repositories
 
         private QuestionsContext context = new QuestionsContext();
 
+        public EFSQLiteQuestionBasesRepository()
+        {
+            using (IDbConnection connection = OpenConnection())
+            {
+                using (IDbCommand createTableCommand = connection.CreateCommand())
+                {
+                    createTableCommand.CommandText = "CREATE TABLE IF NOT EXISTS BasicQuestions(id INTEGER PRIMARY KEY, creation_date TEXT, text TEXT, answer TEXT, is_optional INTEGER)";
+
+                    createTableCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        const string connectionString = @"Data Source=.\data.sqlite";
+
+        private IDbConnection OpenConnection()
+        {
+            IDbConnection connection = new SQLiteConnection(connectionString);
+            connection.Open();
+
+            return connection;
+        }
+
         public void Add(BasicQuestion question)
         {
             context.Questions.Add(question);
@@ -45,7 +70,7 @@ namespace Repositories
 
         public void Clear()
         {
-            context.Database.ExecuteSqlCommand("DELETE FROM QuestionBases");
+            context.Database.ExecuteSqlCommand("DELETE FROM BasicQuestions");
         }
     }
 }
